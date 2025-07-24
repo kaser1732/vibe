d01d471ac03b96d4421b58a3e3bebd1c23556c63
kasi <163301829+kaser1732@users.noreply.github.com>
2025-07-24 09:38:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ cipher = Fernet(ENCRYPTION_KEY)
 st_autorefresh(interval=2000, key="autorefresh")
 
 # 🖼 UI 기본 설정
-st.set_page_config(page_title="🔐 암호화 채팅", layout="centered")
-st.title("🔐 암호화 채팅 앱")
+st.set_page_config(page_title="Chatting", layout="centered")
+st.title("Chatting")
 
 
 # 🚪 나가기 함수 정의
